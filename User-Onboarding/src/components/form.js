5ddec16d32d1612c9a26976c359e55dadd45105e
Ryan-Donovan33 <55716909+Ryan-Donovan33@users.noreply.github.com>
2020-01-23 20:52:02 -0500

    Adding in fields

--- User-Onboarding/src/components/form.js
+++ User-Onboarding/src/components/form.js
@@ -1,10 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import { Form, Field, withFormik } from 'formik';
+import * as Yup from 'yup';
 
 import './Form.scss';
 
-const UserForm = () => {
-	return <div>Form to put added later</div>;
+const UserForm = ({ errors, touched, values, status }) => {
+	const [ user, setUser ] = useState([]);
+
+	return (
+		<div>
+			<h2>User Onboarding</h2>
+			<Form>
+				<Field type="text" name="name" placeholder="Enter Name" value={values.name} />
+				<Field type="email" name="email" placeholder="Email" value={values.email} />
+				<Field type="text" name="pass" placeholder="Password" value={values.pass} />
+				<label>
+					Terms of Services
+					<Field type="checkbox" name="terms" value={values.terms} />
+				</label>
+
+				<button type="submit"> Submit! </button>
+			</Form>
+		</div>
+	);
 };
 
 export default UserForm;
