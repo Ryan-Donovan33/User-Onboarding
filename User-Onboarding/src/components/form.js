ae2a216dd09ce4079878544c36f420751e839539
Ryan-Donovan33 <55716909+Ryan-Donovan33@users.noreply.github.com>
2020-01-23 22:04:41 -0500

    Added in validation and axios request. Still not showing on localhost and need to figure out

--- User-Onboarding/src/components/form.js
+++ User-Onboarding/src/components/form.js
@@ -1,16 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import { Form, Field, withFormik } from 'formik';
 import * as Yup from 'yup';
+import axios from 'axios';
 
 import './Form.scss';
 
 const UserForm = ({ errors, touched, values, status }) => {
 	const [ user, setUser ] = useState([]);
 
+	// useEffect(() => {
+	// 	status && setUser(person => [...person, status]);
+	// }, [status])
+	useEffect(
+		() => {
+			status && setUser((person) => [ ...person, status ]);
+		},
+		[ status ]
+	);
 	return (
 		<div>
 			<h2>User Onboarding</h2>
 			<Form>
+				{/* creating formik form to onboard new users into the system */}
 				<Field type="text" name="name" placeholder="Enter Name" value={values.name} />
 				<Field type="email" name="email" placeholder="Email" value={values.email} />
 				<Field type="text" name="pass" placeholder="Password" value={values.pass} />
@@ -21,8 +32,47 @@ const UserForm = ({ errors, touched, values, status }) => {
 
 				<button type="submit"> Submit! </button>
 			</Form>
+
+			{user.map((el) => (
+				<ul>
+					{/* mapping over and putting only the name and email of the user onto the page */}
+					<li>Name: {el.name}</li>
+					<li>Email: {el.email}</li>
+				</ul>
+			))}
 		</div>
 	);
 };
 
-export default UserForm;
+const FormikUserForm = withFormik({
+	mapPropsToValues({ users }) {
+		//passing props to each field
+		return {
+			name: users || '',
+			email: '',
+			pass: '',
+			terms: false
+		};
+	},
+	// validation required - making sure new users fill out each field. Not all required.
+	validationSchema: Yup.object().shape({
+		name: Yup.string().required('Please Fill in Name!'),
+		email: Yup.string().required('Please put in your email address!'),
+		pass: Yup.stril().required('Password Needed!'),
+		terms: Yup.bool()
+	}),
+
+	handleSubmit(values, { setStatus, resetForm }) {
+		console.log('submitting form:', values);
+
+		axios.post('https://reqres.in/api/users', values);
+		then((res) => {
+			console.log('Success:', res);
+			setStatus(res.data);
+			resetForm();
+		}).catch((err) => {
+			console.log('Error:', err.response);
+		});
+	}
+})(UserForm);
+export default FormikUserForm;
